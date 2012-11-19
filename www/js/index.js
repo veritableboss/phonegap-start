c1a03458f28c84bceb718fc43fb025fbccd63eb6
veritableboss <jboss@thegrubster.com>
2012-11-19 17:45:00 -0600

    adding barcodescanner for the first time

--- www/js/index.js
+++ www/js/index.js
@@ -40,3 +40,15 @@ var app = {
         completeElem.className = completeElem.className.split('hide').join('');
     }
 };
+
+function scan(){
+    window.plugins.barcodeScanner.scan( function(result) {
+        alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+    }, function(error) {
+        alert("Scanning failed: " + error);
+    }
+);
+}
