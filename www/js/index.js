aa7d8af036c20eabf382c23abb4eaa1c2ad2fa15
veritableboss <jboss@thegrubster.com>
2012-11-19 18:08:39 -0600

    Removed duplicate action

--- www/js/index.js
+++ www/js/index.js
@@ -60,25 +60,5 @@ var app = {
             console.log(ex.message);
         }
     }
-    scan: function() {
-        console.log('scanning');
-        try {
-            window.plugins.barcodeScanner.scan(function(args) {
-                console.log("Scanner result: \n" +
-                    "text: " + args.text + "\n" +
-                    "format: " + args.format + "\n" +
-                    "cancelled: " + args.cancelled + "\n");
-                /*
-                if (args.format == "QR_CODE") {
-                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-                }
-                */
-                document.getElementById("info").innerHTML = args.text;
-                console.log(args);
-        });
-        } catch (ex) {
-            console.log(ex.message);
-        }
-    }
 };
 
